baae531fb488b371b07ed31e9d5dc82d1acd10f6
Robert Baykov <baykovr@gmail.com>
2015-10-28 15:37:21 -0500

    Add zsh_profile

--- onos-dev-shell.sh
+++ onos-dev-shell.sh
@@ -5,6 +5,4 @@ export JAVA_HOME=/usr/lib/jvm/java-$JAVA_VERSION-openjdk
 export PATH="$PATH:$JAVA_HOME/bin"
 
 export ONOS_ROOT=$(pwd) 
-source $ONOS_ROOT/tools/dev/bash_profile
-
-alias l=ls
+source $ONOS_ROOT/tools/dev/zsh_profile
